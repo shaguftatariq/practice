742a4f1eb12db0b5c5a3c4184147737228df4636
shaguftatariq <86486872+shaguftatariq@users.noreply.github.com>
2025-05-16 03:33:49 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import BookingForm from './components/BookingForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BookingForm />
+    </div>
+  );
+}
+
+export default App;
